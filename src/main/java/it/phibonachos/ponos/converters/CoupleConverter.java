c6b99f827d8680cdbd5c53991bfa9eca7c7b37a4
Andrea Storace <a.storace@wikicasa.it>
2020-08-22 17:10:34 +0200

    updated ponos javadocs

--- src/main/java/it/phibonachos/ponos/converters/CoupleConverter.java
+++ src/main/java/it/phibonachos/ponos/converters/CoupleConverter.java
@@ -1,5 +1,12 @@
 package it.phibonachos.ponos.converters;
 
+/**
+ * SingleValueConverter is a sample class that shows how fixed arity converter can be implemented starting from {@link MultiValueConverter}
+ * @param <C> Is the return type of the converter
+ * @param <F> Is the first input type of the converter
+ * @param <S> Is the second input type of the converter
+ */
+
 public abstract class CoupleConverter<C,F,S> extends MultiValueConverter<C> {
 
     @Override
@@ -8,5 +15,5 @@ public abstract class CoupleConverter<C,F,S> extends MultiValueConverter<C> {
         return convert((F) objects[0], (S)objects[1]);
     }
 
-    public abstract C convert(F guard, S boundGuard);
+    public abstract C convert(F guard, S boundGuard) throws Exception;
 }
--- src/main/java/it/phibonachos/ponos/converters/SingleValueConverter.java
+++ src/main/java/it/phibonachos/ponos/converters/SingleValueConverter.java
@@ -1,5 +1,10 @@
 package it.phibonachos.ponos.converters;
 
+/**
+ * SingleValueConverter is a sample class that shows how fixed arity converter can be implemented starting from {@link MultiValueConverter}
+ * @param <C> Is the return type of the converter
+ * @param <T> Is the input type of the converter
+ */
 public abstract class SingleValueConverter<C,T> extends MultiValueConverter<C> {
 
     @Override
