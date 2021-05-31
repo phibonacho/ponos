fd01f1ee8a1f7b2a091492add6f269571f584e04
Andrea Storace <andrea.storace.uni@gmail.com>
2021-05-31 22:39:47 +0200

    added default constructor

--- src/main/java/it/phibonachos/ponos/converters/ConverterException.java
+++ src/main/java/it/phibonachos/ponos/converters/ConverterException.java
@@ -1,4 +1,7 @@
 package it.phibonachos.ponos.converters;
 
 public class ConverterException extends RuntimeException{
+    public ConverterException(String message) {
+        super(message);
+    }
 }
